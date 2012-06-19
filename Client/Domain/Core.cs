e116b554dc524f10375278c7ba846c233759475f
ZeroBugBounce <richardlowe@gmail.com>
2012-06-18 23:48:16 -0500

    Added WPF project, Domain and Missive lib

---
+++ Client/Domain/Core.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using ZeroBugBounce.Missive;
+using System.Reflection;
+
+namespace Domain
+{
+	public static class Core
+	{
+		static Messenger messenger;
+
+		public static Messenger Messenger
+		{
+			get
+			{
+				return messenger;
+			}
+		}
+
+		static Core()
+		{
+			messenger = new Messenger();
+		}
+
+		public static void ScanAndLoadHandlers(Assembly assembly)
+		{
+			messenger.ScanAndLoadHandlers(assembly);
+		}
+
+		public static void AddHandlers(params Handler[] handlers)
+		{
+			messenger.AddHandlers(handlers);
+		}
+
+		public static Receipt Send<TMessage, TReply>(TMessage message, Action<TReply> reply)
+		{
+			return messenger.Send<TMessage, TReply>(message, reply);
+		}
+
+		public static Receipt Send<TMessage>(TMessage message)
+		{
+			return messenger.Send<TMessage>(message);
+		}
+	}
+}
